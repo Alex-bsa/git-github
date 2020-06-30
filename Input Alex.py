2abd3b6ddee5b960d9e36636b2ae9992f8c8b869
Alex Seb <alex.mktg40@gmail.com>
2020-06-30 12:26:06 -0700

    Python file addition

---
+++ Input Alex.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Where are you from: ")
+
+print("Hello " + name + "!. Huh still young " + age + "; Cool place: " + location)
